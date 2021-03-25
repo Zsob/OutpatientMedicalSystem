b156e65afe102b8e72ab9e21ce597b2901cbc7c5
极涵 曾 <747039543@qq.com>
2021-03-25 20:23:50 +0800

    1

--- OutpatientSystem/Program.cs
+++ OutpatientSystem/Program.cs
@@ -16,7 +16,7 @@ namespace OutpatientSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new OutpatientForm());
         }
     }
 }
